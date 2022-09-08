3db8c786cf802188d4fc1f56923a082eadf8f36a
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-08 12:07:59 +0200

    Add second.js
    It stores cats names and ages in an array and outputs the data of the ones who are 2 years old.

---
+++ second.js
@@ -0,0 +1,21 @@
+var cats = [];
+for (let index = 0; index < 20; index++) {
+    var obj = {
+        name: "Cirmos"+index,
+        age: Math.round((Math.random()*10)+2,2)
+    };
+    cats.push(obj); 
+}
+
+let youngCounter = 0;
+for (let index = 0; index < cats.length; index++) {
+
+    if(cats[index].age === 2){
+        console.log(cats[index].name+" "+cats[index].age+" éves");
+        youngCounter++;
+    }
+}
+
+if(youngCounter == 0){
+    console.log("Nincs 2 éves macska")
+}
